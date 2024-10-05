44e96239666de183580d806f9cbd67e8f37bb072
VARUN NAIR <137203305+NairVarun1@users.noreply.github.com>
2024-10-05 22:31:32 +0530

    Add files via upload

---
+++ Number Guesser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int attempts = 5;
+
+        Random random = new Random();
+
+        Console.WriteLine("Welcome to the Number Guesser Game ! You have 5 attempts to guess a number between 1 and 10 !");
+
+        int randomNumber = random.Next(1, 11);
+
+        for (int i = 0; i < attempts; i++)
+        {
+            Console.WriteLine("Enter your guess: ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("WOW ! You got it right !!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("That's not right ! Try Again !!");
+            }
+        }
+    }
+}
---
+++ Palindrome Checker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a word or a sentence :");
+        string original = Console.ReadLine().ToLower();
+
+        char[] charArray = original.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+
+        if (reversed == original)
+        {
+            Console.WriteLine("It is a Palindrome");
+        }
+        else
+        {
+            Console.WriteLine("It is not a Palindrome !");
+        }
+    }
+}
